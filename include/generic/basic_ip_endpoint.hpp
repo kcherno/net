150a9f59e9a31d8860655f19e177b8a17ecbf2ca
Kirill Chernigovsky <k.chernigovsky@gmail.com>
2026-05-08 18:57:42 +0800

    add ip endpoint interface

---
+++ include/generic/basic_ip_endpoint.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string_view>
+#include <string>
+
+#include <netinet/in.h>
+
+#include "basic_endpoint.hpp"
+
+namespace net::generic
+{
+    class basic_ip_endpoint
+    {
+    public:
+
+        using port_type = ::in_port_t;
+
+        virtual ~basic_ip_endpoint() = default;
+
+        virtual std::string address() const = 0;
+
+        virtual void address(std::string_view) = 0;
+
+        virtual constexpr port() const noexcept = 0;
+
+        virtual constexpr port(port_type) noexcept = 0;
+    };
+}
